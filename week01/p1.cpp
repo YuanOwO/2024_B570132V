d1252cb464873cbce747d84e27d9e83c6fa33eb8
YuanOwO <yuan@yuanowo.xyz>
2024-03-17 22:54:17 +0800

    Hello World!

---
+++ week01/p1.cpp
@@ -0,0 +1,26 @@
+#pragma GCC optimize("O1")
+#pragma GCC optimize("O2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("inline-functions-called-once")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse", "sse2", "sse3")
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define int long long
+#define endl '\n'
+#define pii pair<int, int>
+#define INF 0x7f7f7f7f7f7f7f7fLL
+
+using namespace std;
+
+signed main() {
+    IOS;
+    cout << "Hello World!" << endl;
+    return 0;
+}
